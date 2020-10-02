b46c8eeb9a03dafa37723bc9ed315a4d9cb214ea
Karel <Karel4ever@gmail.com>
2020-10-02 20:19:21 +0200

    Stats screen header

--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -1,3 +1,5 @@
+import 'package:covid19_dasboard_ui/config.dart/palette.dart';
+import 'package:covid19_dasboard_ui/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class StatsScreen extends StatefulWidget {
@@ -8,6 +10,31 @@ class StatsScreen extends StatefulWidget {
 class _StatsScreenState extends State<StatsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Palette.primaryColor,
+      appBar: CustomAppBar(),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: <Widget>[
+          _buildHeader(),
+        ],
+      ),
+    );
+  }
+
+  SliverPadding _buildHeader() {
+    return SliverPadding(
+      padding: const EdgeInsets.all(20.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          'Statistics',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
